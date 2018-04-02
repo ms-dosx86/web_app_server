4343dd4d96ce175c210ded2aefbd8e1b55dcaa2b
ms-dosx86 <the.bayyr.oorjak@gmail.com>
2018-04-02 15:56:10 +0700

    прибрал всё

--- app.js
+++ app.js
@@ -1,32 +1,21 @@
+//зависимости
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
-const registration = require('./routes/registration');
-const createPlaylist = require('./routes/createPlaylist');
-const auth = require('./routes/auth');
+//зависимости 2
 const config = require('./config');
-const checkEmail = require('./routes/checkEmail');
-const checkLogin = require('./routes/checkLogin');
-
+const user = require('./routes/user/user_route');
+const playlist = require('./routes/playlist/playlist_route');
+//переменные
 const app = express();
 const port = 3000;
-
-app.use(cors(), (req, res, next) => {
-    next();
-});
-app.use(bodyParser.json(), (req, res, next) => {
-    next();
-});
-
+//middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/user', user);
+app.use('/playlist', playlist);
+//подключение к бд
 mongoose.connect(config.database).then(() => console.log('connected'), (e) => console.log(e));
-
-app.post('/user/reg', registration);
-app.post('/user/auth', auth);
-app.get('/user/checkEmail/:email', checkEmail);
-app.get('/user/checkLogin/:login', checkLogin);
-
-app.post('/playlist/create', createPlaylist);
-
+//запуск
 app.listen(port, () => console.log(port + ' is listened'));
