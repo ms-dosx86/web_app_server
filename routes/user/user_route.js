5dd8eefd172e3a0c869664c50b37b9a7676a479d
ms-dosx86 <the.bayyr.oorjak@gmail.com>
2018-04-07 14:14:14 +0700

    getPlaylist и getSettings добавил

--- routes/user/user_route.js
+++ routes/user/user_route.js
@@ -6,11 +6,13 @@ const registration = require('./routes/registration');
 const auth = require('./routes/auth');
 const getUser = require('./routes/getUser');
 const getSettings = require('./routes/getSettings');
+const getPlaylists = require('./routes/getPlaylists');
 
 router.post('/reg', registration);
 router.post('/auth', auth);
 router.get('/checkEmail/:email', checkEmail);
 router.get('/checkLogin/:login', checkLogin);
 router.get('/getUser/:id', getUser);
-router.get('/getSettings/:id');
+router.get('/getSettings/:id', getSettings);
+router.get('/getPlaylists/:id', getPlaylists);
 module.exports = router;
