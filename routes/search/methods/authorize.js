4eed33601d864cea3558841321de377cc90efe9c
ms-dosx86 <the.bayyr.oorjak@gmail.com>
2018-04-13 15:50:58 +0700

    изменил респонз

--- routes/search/methods/authorize.js
+++ routes/search/methods/authorize.js
@@ -14,44 +14,71 @@ fs.mkdir = util.promisify(fs.mkdir);
 fs.writeFile = util.promisify(fs.writeFile);
 
 module.exports = async (credentials, params, res) => {
-    let clientSecret = credentials.installed.client_secret;
-    let clientId = credentials.installed.client_id;
-    let redirectUrl = credentials.installed.redirect_uris[0];
-    let auth = new googleAuth();
-    let oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
-    let token = await fs.readFile(TOKEN_PATH).catch(() => {});
-    if (typeof token === 'undefined') {
-        let authUrl = oauth2Client.generateAuthUrl({
-            access_type: 'offline',
-            scope: SCOPES
-        });
-        console.log('Authorize this app by visiting this url: ', authUrl);
-        let rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
-        rl.question('Enter the code from that page here: ', code => {
-            rl.close();
-            oauth2Client.getToken(code, async (err, token1) => {
-                if (err) throw err;
-                await fs.mkdir(TOKEN_DIR);
-                await fs.writeFile(TOKEN_PATH, JSON.stringify(token1));
-                console.log('Token stored to ' + TOKEN_PATH);
-                token = await fs.readFile(TOKEN_PATH);
-                oauth2Client.credentials = JSON.parse(token.toString('utf8'));
-                let result = await getSearchListByKeyword(oauth2Client, params.searchList);
-                let ids = result.items.map(item => item.id.videoId);
-                params.videos.params.id = ids.join();
-                let videos = await getVideosByIds(oauth2Client, params.videos);
-                res.send(videos);
+    try {
+        let clientSecret = credentials.installed.client_secret;
+        let clientId = credentials.installed.client_id;
+        let redirectUrl = credentials.installed.redirect_uris[0];
+        let auth = new googleAuth();
+        let oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+        let token = await fs.readFile(TOKEN_PATH).catch(() => {});
+        if (typeof token === 'undefined') {
+            let authUrl = oauth2Client.generateAuthUrl({
+                access_type: 'offline',
+                scope: SCOPES
             });
-        })
-    } else {
-        oauth2Client.credentials = JSON.parse(token);
-        let result = await getSearchListByKeyword(oauth2Client, params.searchList);
-        let ids = result.items.map(item => item.id.videoId);
-        params.videos.params.id = ids.join();
-        let videos = await getVideosByIds(oauth2Client, params.videos);
-        res.send(videos);
+            console.log('Authorize this app by visiting this url: ', authUrl);
+            let rl = readline.createInterface({
+                input: process.stdin,
+                output: process.stdout
+            });
+            rl.question('Enter the code from that page here: ', code => {
+                rl.close();
+                oauth2Client.getToken(code, async (err, token1) => {
+                    if (err) throw err;
+                    await fs.mkdir(TOKEN_DIR);
+                    await fs.writeFile(TOKEN_PATH, JSON.stringify(token1));
+                    console.log('Token stored to ' + TOKEN_PATH);
+                    token = await fs.readFile(TOKEN_PATH);
+                    oauth2Client.credentials = JSON.parse(token.toString('utf8'));
+                    let result = await getSearchListByKeyword(oauth2Client, params.searchList);
+                    let ids = result.items.map(item => item.id.videoId);
+                    params.videos.params.id = ids.join();
+                    let videos = await getVideosByIds(oauth2Client, params.videos);
+                    const response = {
+                        success: true,
+                        msg: 'данные были получены',
+                        body: {
+                            nextPageToken: result.nextPageToken,
+                            prevPageToken: result.prevPageToken,
+                            videos: videos
+                        }
+                    }
+                    res.send(videos);
+                });
+            })
+        } else {
+            oauth2Client.credentials = JSON.parse(token);
+            let result = await getSearchListByKeyword(oauth2Client, params.searchList);
+            let ids = result.items.map(item => item.id.videoId);
+            params.videos.params.id = ids.join();
+            let videos = await getVideosByIds(oauth2Client, params.videos);
+            const response = {
+                success: true,
+                msg: 'данные были получены',
+                body: {
+                    nextPageToken: result.nextPageToken,
+                    prevPageToken: result.prevPageToken,
+                    videos: videos
+                }
+            }
+            res.send(response);
+        }
+    } catch (e) {
+        const response = {
+            success: false,
+            msg: e.message
+        }
+        res.send(response);
     }
+    
 }
