b2c49a88cf603835b33d5b42a9112099047ff362
ms-dosx86 <the.bayyr.oorjak@gmail.com>
2018-04-19 11:02:50 +0700

    исправил url

--- app.js
+++ app.js
@@ -15,13 +15,14 @@
         const port = 3000;
         //middleware
         app.use(cors());
+        app.use(bodyParser.urlencoded({ extended: false }));
         app.use(bodyParser.json());
         app.use('/api/user', user);
         app.use('/api/playlist', playlist);
         app.use('/api/search', search);
         //подключение к бд
-        await mongoose.connect(config.database);
-        console.log('connected');
+        // await mongoose.connect(config.database);
+        // console.log('connected');
         await app.listen(port);
         console.log(port + ' is listened');
     } catch(e) {
