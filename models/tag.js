be75f4b830749cc35f6bd9c9e090d4ab080b72fb
ms-dosx86 <the.bayyr.oorjak@gmail.com>
2018-04-27 11:46:19 +0700

    тег, недоделанный

---
+++ models/tag.js
@@ -0,0 +1,9 @@
+// const mongoose = require('mongoose');
+
+// const TagSchema = new mongoose.Schema({
+//     name: String,
+//     count: Number
+// });
+
+// module.exports.TagSchema = TagSchema;
+// module.exports.TagModel = mongoose.Model('Tag', TagSchema);
