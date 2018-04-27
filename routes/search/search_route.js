45a37d8a252285de7d1c9d6c5aa31704f94aa440
ms-dosx86 <the.bayyr.oorjak@gmail.com>
2018-04-27 11:44:05 +0700

    убрал звездочку, добавил поиск видео, и изменил путь для получения списка видео

--- routes/search/search_route.js
+++ routes/search/search_route.js
@@ -1,7 +1,9 @@
 const searchQuery = require('./routes/searchQuery');
+const getVideo = require('./routes/getVideo');
 
 const router = require('express').Router();
 
-router.get('/:query*/:token?', searchQuery);
+router.get('/list/:query/:token?', searchQuery);
+router.get('/video/:id', getVideo);
 
 module.exports = router;
