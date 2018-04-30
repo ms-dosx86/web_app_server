49f9f9cbd674b78d89d2bfa7b5ebef7596b37e9c
ms-dosx86 <the.bayyr.oorjak@gmail.com>
2018-04-30 18:19:02 +0700

    добавил картинку дефолтную для плейлиста при создании

--- routes/playlist/routes/create.js
+++ routes/playlist/routes/create.js
@@ -1,46 +1,83 @@
 const User = require('../../../models/user').UserModel;
 const Playlist = require('../../../models/playlist').PlaylistModel;
+const Tag = require('../../../models/tag').TagModel;
 const formidable = require('formidable');
 const path = require('../../../config').path;
 const fs = require('fs');
 const util = require('util');
 fs.mkdir = util.promisify(fs.mkdir);
 fs.rename = util.promisify(fs.rename);
+fs.readFile = util.promisify(fs.readFile);
+fs.copyFile = util.promisify(fs.copyFile);
 
 module.exports = async (req, res) => {
     try {
         let form = new formidable.IncomingForm();
         form.parse(req, async (err, fields, files) => {
             if (err) throw err;
-            fields.playlist = JSON.parse(fields.playlist);
-            let user = await User.findById(fields.id);
-            if (user === null) throw new Error('пользователь не был найден');
-            const plylist = fields.playlist;
-            plylist.tags.forEach(item => {
-                item = item.toLowerCase()
-            });
-            const playlist = new Playlist({
-                title: plylist.title.toLowerCase(),
-                description: plylist.description.toLowerCase(),
-                img: files.img.name,
-                tags: plylist.tags,
-                list: plylist.tracks
-            });
-            await playlist.save()
-                .catch(e => { throw new Error('ошибка при сохранении плейлиста'); });
-            console.log('плекйлист был создан');
-            let oldpath = files.img.path;
-            let newpath = path + '/files/users/' + fields.id + '/images/playlists/' + files.img.name;
-            await fs.rename(oldpath, newpath);
-            user.playlists.push(playlist);
-            await user.save()
-                .catch(e => { throw new Error('ошибка при добавлении плейлиста в список'); });
-            console.log('playlist was added');
-            const response = {
-                success: true,
-                msg: 'playlist was created'
+            console.log(fields)
+            if (fields.playlist === 'undefined') {
+                const response = {
+                    success: false,
+                    msg: 'plalist was undefined'
+                }
+                res.send(response);
+            } else {
+                fields.playlist = JSON.parse(fields.playlist);
+                let user = await User.findById(fields.id);
+                if (user === null) throw new Error('пользователь не был найден');
+                const plylist = fields.playlist;
+                let tags = [];
+                for (let i = 0; i < plylist.tags.length; i++) {
+                    let t = await Tag.findOne({ name: plylist.tags[i].toLowerCase() });
+                    if (t === null) {
+                        let tag = new Tag({
+                            name: plylist.tags[i].toLowerCase()
+                        });
+                        await tag.save();
+                        tags.push(tag);
+                    } else {
+                        t.count++;
+                        await t.save();
+                        tags.push(t);
+                    }
+                }
+                const playlist = new Playlist({
+                    title: plylist.title.toLowerCase(),
+                    description: plylist.description.toLowerCase(),
+                    img: '',
+                    tags: tags,
+                    list: plylist.tracks
+                });
+                await playlist.save()
+                    .catch(e => { throw new Error('ошибка при сохранении плейлиста'); });
+                console.log('плекйлист был создан');
+                if (typeof files.img == 'undefined') {
+                    let from = path + '/defaults/default.jpg';
+                    let to = path + '/files/users/' + fields.id + '/images/playlists/' + playlist._id + '.jpg';
+                    await fs.copyFile(from, to);
+                    playlist.img = playlist._id + '.jpg';
+                } else {
+                    let type = '';
+                    files.img.type == 'image/png' ? type = '.png' : type = '.jpg';
+                    let oldpath = files.img.path;
+                    let newpath = path + '/files/users/' + fields.id + '/images/playlists/' + playlist._id + type;
+                    await fs.rename(oldpath, newpath);
+                    playlist.img = playlist._id + type;
+                }
+                await playlist.save()
+                    .catch(e => { throw new Error('ошибка при сохранении плейлиста'); });
+                console.log('плекйлист был перезаписан');  
+                user.playlists.push(playlist);
+                await user.save()
+                    .catch(e => { throw new Error('ошибка при добавлении плейлиста в список'); });
+                console.log('playlist was added');
+                const response = {
+                    success: true,
+                    msg: 'playlist was created'
+                }
+                res.send(response);
             }
-            res.send(response);
         })
     } catch (e) {
         const response = {
