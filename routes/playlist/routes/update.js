abb5932100bd4d88cfa20417287e85f19739542e
ms-dosx86 <the.bayyr.oorjak@gmail.com>
2018-05-01 15:23:58 +0700

    сделал update плейлиста полностью

--- routes/playlist/routes/update.js
+++ routes/playlist/routes/update.js
@@ -1,26 +1,72 @@
 const Playlist = require('../../../models/playlist').PlaylistModel;
 const User = require('../../../models/user').UserModel;
+const Tag = require('../../../models/tag').TagModel;
+const formidable = require('formidable');
+const path = require('../../../config').path;
+const sharp = require('sharp');
+const fs = require('fs')
 
 module.exports = async (req, res) => {
     try {
-        console.log(req.body);
-        let user = await User.findById(req.body.userid);
-        if (user === null) throw new Error('пользователь не найден');
-        let playlist = await Playlist.findById(req.body.playlist._id);
-        if (playlist === null) throw new Error('плейлист не найден');
-        for (let i = 0; i < user.playlists.length; i++) {
-            if (user.playlists[i]._id == req.body.playlist._id) {
-                user.playlists[i] = req.body.playlist;
-                await user.save();
-                break;
+        let form = new formidable.IncomingForm();
+        form.parse(req, async (err, fields, files) => {
+            if (err) throw err;
+            fields.playlist = JSON.parse(fields.playlist);
+            console.log(fields)
+            console.log(files)
+            let user = await User.findById(fields.id);
+            if (user === null) throw new Error('user not found');
+            let pl1 = await Playlist.findById(fields.playlist._id);
+            if (pl1 === null) throw new Error('playlist not found');
+            let pl = fields.playlist;
+            for (let i = 0; i < pl.tags.length; i++) {
+                let t = await Tag.findOne({ name: pl.tags[i] });
+                if (t === null) {
+                    let t = new Tag({
+                        name: pl.tags[i]
+                    });
+                    await t.save();
+                    pl.tags[i] = t;
+                } else {
+                    t.count++;
+                    await t.save();
+                    pl.tags[i] = t;
+                }
             }
-        }
-        await Playlist.findByIdAndUpdate(req.body.playlist._id, req.body.playlist);
-        const response = {
-            success: true,
-            msg: 'плейлист обновлен'
-        }
-        res.send(response);
+            if (typeof files.img == 'undefined') {
+                // let from = path + '/defaults/default.jpg';
+                // let to = path + '/files/images/playlists/' + playlist._id + '.jpg';
+                // await fs.copyFile(from, to);
+            } else {
+                console.log('object');
+                let type = '';
+                files.img.type == 'image/png' ? type = '.png' : type = '.jpg';
+                console.log(type);
+                let oldpath = files.img.path;
+                let newpath = path + '/files/temp/' + pl._id + type;
+                await fs.rename(oldpath, newpath);
+                console.log('was moved to temp');
+                let img = await sharp(path + '/files/temp/' + pl._id + type);
+                console.log(pl._id + type)
+                let i = await img.resize(500, 500).toFile(path + '/files/images/playlists/' + pl._id + type);
+                pl1.img = pl._id + type;
+                console.log(`img was resized`)                    
+            }
+            pl1.title = pl.title;
+            pl1.description = pl.description;
+            pl1.tags = pl.tags;
+            pl1.list = pl.list;
+            await pl1.save();
+            console.log('playlist updated');
+            for (let i = 0; i < user.playlists.length; i++) {
+                if (user.playlists[i]._id == pl1._id) {
+                    user.playlists[i] = pl1;
+                }
+            }
+            await user.save();
+            console.log('user updated');
+            res.end();
+        })
     } catch (e) {
         const response = {
             success: false,
