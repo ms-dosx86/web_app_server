ba8d5bcc6ddb851b6d7e588c4f442955208315f5
ms-dosx86 <the.bayyr.oorjak@gmail.com>
2018-05-16 17:48:14 +0700

    получить плейлисты юзера

--- routes/user/routes/getPlaylists.js
+++ routes/user/routes/getPlaylists.js
@@ -5,10 +5,24 @@ module.exports = async (req, res) => {
     try {
         let user = await User.findById(req.params.id);
         if (user === null) throw new Error('нет такого пользователя');
+        let page = req.params.page;
+        let playlists = [];
+        let pages = 0;
+        if (user.playlists) {
+            let i = user.playlists.length - page*10 - 1;
+            pages = (user.playlists.length / 10).toFixed();
+            while (playlists.length < 10 && i > 0) {
+                playlists.push(user.playlists[i]);
+                i--;
+            } 
+        }
         const response = {
             success: true,
             msg: 'пользователь найден',
-            body: user.playlists
+            body: {
+                playlists: playlists,
+                allPages: pages
+            }
         }
         res.send(response);
     } catch (e) {
