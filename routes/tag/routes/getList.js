6a01eb5c75eeac1f4de2fe7685e3959c9af1ffe1
ms-dosx86 <the.bayyr.oorjak@gmail.com>
2018-05-16 22:06:23 +0700

    ограничил список выпадающих тегов до 10

--- routes/tag/routes/getList.js
+++ routes/tag/routes/getList.js
@@ -5,10 +5,16 @@ module.exports = async (req, res) => {
         let name = req.params.name;
         let re = new RegExp('^(' + name + ')', 'ig');
         let tags = await Tag.find({name: re});
+        let i = 0;
+        let arrTags = [];
+        while (arrTags.length < 10 && i < tags.length) {
+            arrTags.push(tags[i]);
+            i++;
+        }
         const response = {
             success: true,
             msg: 'tags given',
-            body: tags
+            body: arrTags
         }
         res.send(response);
     } catch (e) {
