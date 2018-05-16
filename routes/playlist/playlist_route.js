bd278553f2515ff65f17e5443668de2576156895
ms-dosx86 <the.bayyr.oorjak@gmail.com>
2018-05-16 22:08:05 +0700

    добавил необзяталельный параметр view и обязательный page

--- routes/playlist/playlist_route.js
+++ routes/playlist/playlist_route.js
@@ -8,10 +8,10 @@ const getAll = require('./routes/getAll');
 const search = require('./routes/search');
 
 router.post('/create', create);
-router.get('/id/:id', get);
+router.get('/id/:id/:view?', get);
 router.post('/update', update);
 router.get('/delete/:userid/:playid', deletePlaylist);
 router.get('/list/getAll', getAll);
-router.post('/search', search);
+router.post('/search/:page', search);
 
 module.exports = router;
