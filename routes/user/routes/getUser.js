2ccf99eb0e6440c6fd8fe74c37881b8a0d098632
ms-dosx86 <the.bayyr.oorjak@gmail.com>
2018-05-22 13:50:00 +0700

    теперь возвращает юзера без плейлистов

--- routes/user/routes/getUser.js
+++ routes/user/routes/getUser.js
@@ -4,10 +4,17 @@ module.exports = async (req, res) => {
     try {
         const user = await User.findById(req.params.id);
         if (user === null) throw new Error(`пользователь не был найден`);
+        let usr = {
+            login: user.login,
+            email: user.email,
+            _id: user._id,
+            img: user.img,
+            status: user.status
+        }
         const response = {
             success: true,
             msg: 'юзер найден',
-            body: user
+            body: usr
         }
         res.send(response);
     } catch (e) {
