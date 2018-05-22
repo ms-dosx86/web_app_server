73136c953339bdc407fcbb71e83a509763572262
ms-dosx86 <the.bayyr.oorjak@gmail.com>
2018-05-22 14:38:03 +0700

    получение юзера по логину

---
+++ routes/user/routes/getUserByLogin.js
@@ -0,0 +1,27 @@
+const User = require('../../../models/user').UserModel;
+
+module.exports = async (req, res) => {
+    try {
+        const user = await User.findOne({ login: req.params.login });
+        if (user === null) throw new Error(`пользователь не был найден`);
+        let usr = {
+            login: user.login,
+            email: user.email,
+            _id: user._id,
+            img: user.img,
+            status: user.status
+        }
+        const response = {
+            success: true,
+            msg: 'юзер найден',
+            body: usr
+        }
+        res.send(response);
+    } catch (e) {
+        const response = {
+            success: false,
+            msg: e.message
+        }
+        res.send(response);
+    }
+}
