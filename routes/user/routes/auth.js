d0ef3a71fb53c6c2d6138acf119dbab6c9afd009
ms-dosx86 <the.bayyr.oorjak@gmail.com>
2018-05-27 22:13:39 +0700

    добавил соль

--- routes/user/routes/auth.js
+++ routes/user/routes/auth.js
@@ -3,8 +3,10 @@ const sha1 = require('sha1');
 
 module.exports = async (req, res) => {
     try {
-        const user = await User.findOne({ login: req.body.login.toLowerCase(), password: sha1(req.body.password) });
-        if (user === null) throw new Error(`юзер не был найден`);
+        const user = await User.findOne({ login: req.body.login.toLowerCase() });
+        if (user === null) throw new Error('юзер не был найден');
+        let password = sha1(req.body.password + user.salt);
+        if (user.password != password) throw new Error('неправильный пароль');
         const response = {
             success: true,
             msg: 'юзер найден',
--- routes/user/routes/registration.js
+++ routes/user/routes/registration.js
@@ -3,24 +3,41 @@ const sha1 = require('sha1');
 const fs = require('fs');
 const path = require('../../../config').path;
 const util = require('util');
+const crypto = require('crypto');
 fs.mkdir = util.promisify(fs.mkdir);
+fs.copyFile = util.promisify(fs.copyFile);
+
+function exists(path) {
+    return new Promise(resolve => {
+        fs.exists(path, resolve);
+    })
+}
 
 module.exports = async (req, res) => {
     try {
+        const salt = crypto.randomBytes(32).toString('hex');
         const user = new User({
             login: req.body.login.toLowerCase(),
             email: req.body.email.toLowerCase(),
-            password: sha1(req.body.password),
+            password: sha1(req.body.password + salt),
+            salt: salt,
             img: '',
             playlists: [],
             favs: []
         });
+        let isExist = await exists(path + '/files');
+        if (!isExist) {
+            await fs.mkdir(path + '/files');
+            await fs.mkdir(path + '/files/images');
+            await fs.mkdir(path + '/files/images/avatars');
+            await fs.mkdir(path + '/files/images/playlists');
+            await fs.mkdir(path + '/files/temp');
+        }
         let from = path + '/defaults/avatar_default.jpg';
         let to = path + '/files/images/avatars/' + user._id + '.jpg';
         await fs.copyFile(from, to);
         user.img = user._id + '.jpg';
-        await user.save()
-            .catch(e => { throw new Error('ошибка при сохранении юзера'); });
+        await user.save().catch(e => { throw new Error('ошибка при сохранении юзера'); });
         console.log(`${user.login} is saved`);
         const response = {
             success: true,
@@ -31,7 +48,7 @@ module.exports = async (req, res) => {
     } catch (e) {
         const response = {
             success: false,
-            msg: e
+            msg: e.message
         }
         res.send(response);
     }
