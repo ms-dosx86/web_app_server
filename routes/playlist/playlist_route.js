182273c2f2be81d5cfb717634d23fa0c20f5559e
ms-dosx86 <the.bayyr.oorjak@gmail.com>
2018-06-02 00:08:27 +0700

    удалил роуты, которые изменяли только название и описание плейлиста, и добавил роут для полного обновления плейлиста

--- routes/playlist/playlist_route.js
+++ routes/playlist/playlist_route.js
@@ -2,18 +2,16 @@ const express = require('express');
 const router = express.Router();
 const create = require('./routes/create');
 const get = require('./routes/get');
-const changeTitle = require('./routes/changeTitle');
 const deletePlaylist = require('./routes/delete');
 const getAll = require('./routes/getAll');
 const search = require('./routes/search');
-const changeDesc = require('./routes/changeDesc');
+const update = require('./routes/update');
 
 router.post('/create', create);
 router.get('/id/:id/:view?', get);
-router.post('/changeTitle', changeTitle);
 router.get('/delete/:userid/:playid', deletePlaylist);
 router.get('/list/getAll', getAll);
 router.post('/search/:page', search);
-router.post('/changeDesc', changeDesc);
+router.post('/update', update);
 
 module.exports = router;
