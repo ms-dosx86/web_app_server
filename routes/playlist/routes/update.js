e9afe12223c04e03b59ffb71015b219fbd6f0983
ms-dosx86 <the.bayyr.oorjak@gmail.com>
2018-06-04 23:58:17 +0700

    сделал обновление изображения, но пока не до конца корректно работает, ит шуд би фиксед сун

--- routes/playlist/routes/update.js
+++ routes/playlist/routes/update.js
@@ -1,5 +1,12 @@
 const Playlist = require('../../../models/playlist').PlaylistModel;
 const User = require('../../../models/user').UserModel;
+const formidable = require('formidable');
+const path = require('../../../config').path;
+const fs = require('fs');
+const util = require('util');
+const sharp = require('sharp');
+fs.copyFile = util.promisify(fs.copyFile);
+fs.unlink = util.promisify(fs.unlink);
 
 function findIndexById(id, arr) {
     for (let i = 0; i < arr.length; i++) {
@@ -10,37 +17,65 @@ function findIndexById(id, arr) {
     return -1;
 }
 
+function existsAsync(path) {
+    return new Promise(function (resolve) {
+        fs.exists(path, resolve);
+    });
+}
+
 module.exports = async (req, res) => {
     try {
-        let playlistPrev = req.body;
-        let user = await User.findById(playlistPrev.userId);
-        let playlist = await Playlist.findById(playlistPrev._id);
-        if (user && playlist) {
-            let index = findIndexById(playlistPrev._id, user.playlists);
-            if (index != -1) {
-                for (let key in playlistPrev) {
-                    if (playlistPrev[key].isUpdated) {
-                        playlist[key] = playlistPrev[key].value;
-                        console.log(user.playlists[index][key]);
-                        user.playlists[index][key] = playlistPrev[key].value;
-                        console.log(user.playlists[index][key]);
+        let form = new formidable.IncomingForm();
+        form.parse(req, async (err, fields, files) => {
+            if (err) throw err;
+            fields.playlist = JSON.parse(fields.playlist);
+            let playlistPrev = fields.playlist;
+            let user = await User.findById(fields.userId);
+            let playlist = await Playlist.findById(playlistPrev._id);
+            if (user && playlist) {
+                let index = findIndexById(playlistPrev._id, user.playlists);
+                if (~index) {
+                    for (let key in playlistPrev) {
+                        if (playlistPrev[key].isUpdated) {
+                            playlist[key] = playlistPrev[key].value;
+                            user.playlists[index][key] = playlistPrev[key].value;
+                        }
                     }
+                    if (files.img) {
+                        let prevImg = path + '/files/images/playlists/' + playlist.img;
+                        let prevTempImg = path + '/files/temp/' + playlist.img;
+                        let exists = await existsAsync(prevImg);
+                        exists && await fs.unlink(prevImg);
+                        exists = await existsAsync(prevTempImg);
+                        exists && await fs.unlink(prevTempImg);
+                        let type;
+                        files.img.type = 'image/png' ? type = '.png' : type = '.jpg';
+                        let oldpath = files.img.path;
+                        let newpath = path + '/files/temp/' + playlist._id + type;
+                        await fs.copyFile(oldpath, newpath);
+                        console.log('image was moved to temp');
+                        let img = await sharp(path + '/files/temp/' + playlist._id + type);
+                        let i = await img.resize(500, 500).toFile(path + '/files/images/playlists/' + playlist._id + type);
+                        console.log('image was resized');
+                        playlist.img = playlist._id + type;
+                        user.playlists[index]['img'] = playlist._id + type;
+                    }
+                    await playlist.save();
+                    await user.save();
+                    console.log('playlist was updated');
+                    const response = {
+                        success: true,
+                        msg: 'playlist was updated'
+                    }
+                    res.send(response);
+                } else {
+                    throw new Error('playlist not found');
                 }
-                await playlist.save();
-                console.log('playlist was updated');
-                await user.save();
-                console.log('user\' playlist was updated');
-                const response = {
-                    success: true,
-                    msg: 'playlist was updated'
-                }
-                res.send(response);
             } else {
-                throw new Error('playlist not found in user doc')
+                throw new Error('playlist not found');
             }
-        } else {
-            throw new Error('playlist or user is not found');
-        }
+            res.end();
+        })
     } catch (e) {
         const response = {
             success: false,
