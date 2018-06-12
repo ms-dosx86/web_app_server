44dfab22741ec91370ef63ae0914b085771def29
ms-dosx86 <the.bayyr.oorjak@gmail.com>
2018-06-12 21:47:57 +0700

    добавил let

--- routes/home/home_route.js
+++ routes/home/home_route.js
@@ -11,7 +11,7 @@ router.get('/', async (req, res) => {
         let top_playlists = await Playlist.find().sort({ viewCount: -1 }).limit(8);
         let top_tags = await Tag.find().sort({ count: -1 }).limit(3);
         let by_tags = [];
-        for (item of top_tags) {
+        for (let item of top_tags) {
             let list = await Playlist.find({
                 tags: {
                     $in: [item]
